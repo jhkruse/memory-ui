929be6c1e92bfc846cb9b7b567c0cfcbbdc9c040
Jan-Hendrik Kruse <jan-hendrik.kruse@finanzcheck.de>
2019-07-08 15:52:25 +0200

    app-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardListComponent } from './card-list/card-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardListComponent
   ],
   imports: [
     BrowserModule,
