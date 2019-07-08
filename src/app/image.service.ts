238f490a2cf61275c31270dda944d119f9af629c
Jens Krefeldt <jens.krefeldt@finanzcheck.de>
2019-07-08 21:06:22 +0200

    image service first test

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {
+  Observable,
+  throwError,
+} from 'rxjs';
+import {
+  catchError,
+  retry,
+  map,
+} from 'rxjs/operators';
 
 const BASE_URL = 'https://picsum.photos';
 const LIST_URL = `${BASE_URL}/v2/list`;
@@ -17,11 +27,57 @@ interface PicsumImage {
 })
 export class ImageService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  async createRandomImageUrls(imageSize: number = 200, imageAmount: number = 5): Promise<Array<string>> {
-    const result = await fetch(`${LIST_URL}?limit=${imageAmount}`);
-    const images: Array<PicsumImage> = await result.json();
-    return images.map(image => `${BASE_URL}/id/${image.id}/${imageSize}`);
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
+  }
+
+  /**
+   * Randomly shuffle an array, see: https://stackoverflow.com/a/2450976/1293256
+   */
+  private shuffle(array: string[]): string[] {
+    let currentIndex = array.length;
+    let temporaryValue: string;
+    let randomIndex: number;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  getShuffledMemoryImages(imageSize: number = 200, imageAmount: number = 5): Observable<string[]> {
+    return this.http
+      .get<PicsumImage[]>(`${LIST_URL}?limit=${imageAmount}`)
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        map((images: PicsumImage[]) => {
+          let imageUrls = images.map(image => `${BASE_URL}/id/${image.id}/${imageSize}`);
+          imageUrls = imageUrls.concat(imageUrls);
+          return this.shuffle(imageUrls);
+        }),
+        catchError(this.handleError)
+      );
   }
 }
