37d5d2b71dbbae1b71f76ab0999fc0ab2b4be78f
Jens Krefeldt <jens.krefeldt@finanzcheck.de>
2019-07-11 10:49:03 +0200

    Improve pair settings model

--- src/app/start-view/start-view.component.ts
+++ src/app/start-view/start-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { Router } from '@angular/router';
@@ -11,13 +11,22 @@ export class FormErrorStateMatcher implements ErrorStateMatcher {
   }
 }
 
+/**
+ * Maximum amount of pairs: 12 pairs => 4 to 28 cards.
+ */
+const MAX_AMOUNT_PAIRS = 12;
+
 @Component({
   selector: 'app-start-view',
   templateUrl: './start-view.component.html',
   styleUrls: ['./start-view.component.css']
 })
-
 export class StartViewComponent implements OnInit {
+  /**
+   * The options to choose for amount of cards on the board.
+   */
+  @Input() cardAmounts: number[];
+
   playerOneFormControl = new FormControl('', [
     Validators.required,
     Validators.minLength(4),
@@ -32,7 +41,12 @@ export class StartViewComponent implements OnInit {
 
   matcher = new FormErrorStateMatcher();
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    this.cardAmounts = [];
+    for (let index = 1; index <= MAX_AMOUNT_PAIRS; index++) {
+      this.cardAmounts.push((index + 1) * 2);
+    }
+  }
 
   ngOnInit() {
   }
